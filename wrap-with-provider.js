27c44bdaab2ac1265b7f728281a4e31952aad4fa
quidcain <quidcain@gmail.com>
2020-01-12 02:25:31 +0300

    Fix production build

--- wrap-with-provider.js
+++ wrap-with-provider.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import createStore from './src/state/createStore';
+import { isBrowser } from '@app/utils';
 
 export default ({ element }) => {
   const store = createStore();
-  if(window) {
+  if(isBrowser()) {
     window.store = store;
   }
   return <Provider store={store}>{element}</Provider>
